527a4d1575ed56d449804b2abfe1927d674e5171
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-02 04:02:28 +0100

    task5

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Add auth header."""
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        return request.headers["Authorization"]
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get current user."""
