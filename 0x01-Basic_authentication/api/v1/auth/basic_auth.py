434767caa9ce6c4716cf644e21bc6be27dee88e6
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-04 06:02:48 +0100

    fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,12 +2,10 @@
 """Basic Auth module."""
 
 
-from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 from api.v1.auth.auth import Auth
+import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
