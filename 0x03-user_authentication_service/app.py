4e1183f2a2dd2a38c421175f72ac984e633fb4fc
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-09 00:21:31 +0100

    Task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,34 @@
 """
 Flask app module
 """
-from flask import Flask, jsonify
+
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
+
 
 @app.route("/")
 def welcome():
+    """Home route"""
     message = {"message": "Bienvenue"}
     return jsonify(message)
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """Register new User."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
