129e40bcbf96ec4061d1887b1a379ff2f12ad263
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-09 22:53:43 +0100

    fixed syntax error on encrypt password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,10 +7,9 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """Generate a salt and Hash password."""
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Check if the provided password matches the hashed password."""
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
