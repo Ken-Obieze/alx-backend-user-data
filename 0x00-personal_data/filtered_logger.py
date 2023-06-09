4e1a68d11bb88f795ecd303a01f8f36b878c0c58
Ken-Obieze <kenneth.obieze@outlook.com>
2023-06-09 22:21:10 +0100

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,8 @@ Filtered Logger
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Replace occurrences of certain field values with the redaction string.
 
@@ -14,10 +15,13 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         fields: List of strings representing fields to obfuscate.
         redaction: String representing the obfuscation value.
         message: String representing the log line.
-        separator: String representing the character separating the fields in the log line.
+        separator: String representing the character separating the fields
+                   in the log line.
 
     Returns:
         The log message with obfuscated fields.
     """
-    regex = re.compile('|'.join(fields))
-    return regex.sub(redaction, message)
+    for key in fields:
+        pattern = r'({0}=)[^{1}]*({1})'.format(key, separator)
+        message = re.sub(pattern, r'\1{}\2'.format(redaction), message)
+    return message
