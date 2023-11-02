794559a30cef3538c154a7c267124ca44f19a128
Ken-Obieze <kenneth.obieze@outlook.com>
2023-11-02 20:41:31 +0100

    blk

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """Filtered Logger."""
 
+import os
 import logging
+import mysql.connector
 import re
-from typing import List
+from typing import List, Tuple
 
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS: Tuple[str]= ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -40,9 +42,11 @@ class RedactingFormatter(logging.Formatter):
 
     def format(self, record: logging.LogRecord) -> str:
         """Format the LogRecord instanceFormats the LogRecord instance."""
-        log = super(RedactingFormatter, self).format(record=record)
-        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
-
+        message = super().format(record)
+        for field in self.fields:
+            message = filter_datum([field], \
+                self.REDACTION, message, self.SEPARATOR)
+        return message
 
 def get_logger():
     """Create and configures a logger."""
@@ -57,3 +61,50 @@ def get_logger():
     logger.addHandler(stream_handler)
 
     return logger
+
+def get_db():
+    """Connect to a mysql database."""
+    username = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.environ.get("PERSONAL_DATA_DB_NAME")
+
+    connection = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+
+    return connection
+
+
+def main():
+    """Log database users."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    for row in cursor:
+        filtered_data = ""
+        for field in PII_FIELDS:
+            filtered_data += f"{field}={formatter.REDACTION}; "
+        remaining_data = [f"{field}={value}" for field, value in zip(cursor.column_names, row)]
+        log_message = f"{filtered_data}{'; '.join(remaining_data)}"
+        logger.info(log_message)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
