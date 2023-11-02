fbe363cb0dd67f8fee7d2e2a0066c9ffd99a188e
Ken-Obieze <kenneth.obieze@outlook.com>
2023-11-02 19:30:33 +0100

    task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Filtered Logger."""
 
+import logging
 import re
 from typing import List
 
@@ -21,3 +22,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     """
     regex = '|'.join(map(re.escape, fields))
     return re.sub(f'({regex})=[^\\{separator}]*', f'\\1={redaction}', message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        """Instantiation method, sets fields for each instance."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the LogRecord instanceFormats the LogRecord instance."""
+        message = super().format(record)
+        for field in self.fields:
+            message = filter_datum([field], self.REDACTION, message, self.SEPARATOR)
+        return message
