599298e16c038abf9d84bb9e9bc4400563947333
Ken-Obieze <kenneth.obieze@outlook.com>
2023-11-02 20:51:49 +0100

    pwsswrd hardh complete

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,6 +3,7 @@
 
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """
     Hash and salt a password using bcrypt.
@@ -16,3 +17,16 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate if the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): Salted and hashed password as a byte string.
+        password (str): The plain-text password to be validated.
+
+    Returns:
+        bool: True if password matched, otherwise False.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
